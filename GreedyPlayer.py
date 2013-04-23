16f532f04574ef24071f3e4a58a1c2a8e78ad7f3
Jens Schmer <der.liggers@gmail.com>
2013-04-23 14:22:22 +0200

    pick a random 'best' move

--- GreedyPlayer.py
+++ GreedyPlayer.py
@@ -3,6 +3,7 @@
 
 from Board import Board
 import copy
+from random import choice
 
 class GreedyPlayer(object):
     def generate_move(self, game):
@@ -14,6 +15,7 @@ class GreedyPlayer(object):
         legal_moves = game.legal_moves()
         assert legal_moves
 
+        # calculate a score for each move
         scored_moves = []
         for move in legal_moves:
             board = copy.deepcopy(game)
@@ -21,5 +23,17 @@ class GreedyPlayer(object):
             score = board.score()
             scored_moves.append((score, move))
 
+        # sort the moves on their score
         scored_moves = sorted(scored_moves, key=lambda t: t[0])
-        return scored_moves[0][1]
+
+        # extract the best moves
+        best_moves = []
+        best_score = scored_moves[0][0]
+        for scored_move in scored_moves:
+            if scored_move[0] == best_score:
+                best_moves.append(scored_move)
+            else:
+                break
+
+        # pick a random move out of the best ones
+        return choice(best_moves)[1]
