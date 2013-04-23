2f7cc485993aa9d255695cc936b0f87761de2a7d
Michael Engelhard <engelhard.michael@gmail.com>
2013-04-23 11:39:08 +0200

    Added human and random player.

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -1,46 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2013 Jens Schmer, Michael Engelhard
-
-from Board import Board
 from Move import Move
 
-game = Board()
-
-while True:
-    print("\n")
-    print(game)
-    a = input("Cmd: ").strip()
-
-    if not a:
-        continue
-    if a == "exit":
-        break
-
-    try:
-        human_move = Move.from_string(a)
-        legal_moves = game.legal_moves()
-        
-        if not human_move in legal_moves:
-            raise ValueError("Invalid move!")
-
-        status = game.move(human_move)
-
-        if status == 'W':
-            # white wins
-            print("White wins! Congrats!")
-            break
-        elif status == 'B':
-            # black wins
-            print("Black wins! Congrats!")
-            break
-        elif status == '=':
-            # draw
-            print("A boring draw...")
-            break
-        else:
-            # nothing happend, doh!
-            pass
-    except ValueError:
-        print("Invalid move! Try again!")
-
-print(game)
+class HumanPlayer(obeject):
+	def generate_move(game):
+        """
+        Generate and return a move for the current turn color.
+        The caller ensures that a legal move exists.
+        If server checks if the move was legal and calls the function again if not.
+        """
+        a = input("Your move: ").strip()
+		move = Move.from_string(a)
+        assert legal_moves
+        return move
--- RandomPlayer.py
+++ RandomPlayer.py
@@ -1,40 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2013 Jens Schmer, Michael Engelhard
-
-from Board import Board
 from Move import Move
 from random import choice
 
-game = Board()
-
-while True:
-    print("\n")
-    print(game)
-
-    legal_moves = game.legal_moves()
-       
-    if not legal_moves:
-        winner = "Black" if game.turn == "W" else "White"
-        print(winner + " wins!")
-        break
-     
-    move = choice(legal_moves)
-    status = game.move(move)
-
-    if status == 'W':
-        # white wins
-        print("White wins with " + str(move) + "! Congrats!")
-        break
-    elif status == 'B':
-        # black wins
-        print("Black wins with " + str(move) + "! Congrats!")
-        break
-    elif status == '=':
-        # draw
-        print("A boring draw...")
-        break
-    else:
-        # nothing happend, doh!
-        pass
-
-print(game)
+class RandomPlayer(obeject):
+    def generate_move(game):
+        """
+        Generate and return a move for the current turn color.
+        The caller ensures that a legal move exists.
+        """
+        legal_moves = game.legal_moves()
+        assert legal_moves
+        return choice(legal_moves)
